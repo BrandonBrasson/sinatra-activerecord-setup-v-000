107c744907411912c056d6daf616b3fe664f9cab
Brandon Brasson <rocksong622@gmail.com>
2019-12-01 23:58:34 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,9 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-configure :development do
-  set :database, 'sqlite3:db/database.db'
-end
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
 
-configure :development do
-  set :database, 'sqlite3:db/create_user.db'
-end
-require './app'
+require_all 'app'
